92e8a9a3bcd5ddf5b76656e3289c5efd242f31e4
Rimian Perkins <rimian.perkins@marketplacer.com>
2022-03-11 14:18:07 +1100

    if block giveneth

--- spec/smoke_test_spec.rb
+++ spec/smoke_test_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 RSpec.configure do |config|
   def my_context_helper(&block)
-    block.call
+    block.call if block_given?
   end
 end
 
 RSpec.describe 'Smoke Test' do
+  my_context_helper
+
   my_context_helper do
+
   end
 end
